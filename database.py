d012a86b1a29165942e652b60c7bf2722f03cfff
Denys Zazuliak (12KG) <DenZaz19@highgateschool.org.uk>
2025-01-20 14:33:37 +0000

    Initial commit

---
+++ database.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+#from faker import Faker
+#import random
+
+#fake = Faker('en_GB')
+
+#conn=sqlite3.connect('student.sqlite')
+
+with sqlite3.connect('database.db') as conn:
+    cursor=conn.cursor()
+
+#    create_students_table="""
+#    CREATE TABLE IF NOT EXISTS students(
+#        id INTEGER PRIMARY KEY AUTOINCREMENT,
+#        firstname TEXT NOT NULL,
+#        lastname TEXT NOT NULL,
+#        age INTEGER,
+#        gender TEXT);
+#    """
+#    cursor.execute(create_students_table)
+
+#    insert_query="""
+#    INSERT INTO students(firstname, lastname, age, gender)
+#    VALUES ('Milan','Gal',17,'M'),
+#    ('Rayhan','Chowdhury',16,'M'),
+#    ('Eleanore','Shiner',16,'F'),
+#    ('Adam','Reeves',16,'M'),
+#    ('Sami','Hafezjee',16,'M');
+#    """
+#    cursor.execute(insert_query)
+
+#    parameterised_insert_query="""
+#    INSERT INTO students (firstname, lastname, age, gender)
+#    VALUES (?, ?, ?, ?);
+#    """
+    #cursor.execute(parameterised_insert_query, ('Ethan','Levy', 16, 'M'))
+
+#    fake.random.seed(6082008)
+#    random.seed(6082008)
+#    for _ in range(10):
+#        f_name=fake.first_name()
+#        l_name=fake.last_name()
+#        age=random.randint(16,17)
+#        gender=random.choice(('M','F'))
+#        cursor.execute(parameterised_insert_query,(f_name,l_name,age,gender))
+
+    conn.commit()
+
+#conn.close()
